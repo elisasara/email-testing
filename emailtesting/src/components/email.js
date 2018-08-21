058ccb730201674432ac9b758f2b2767c88fda10
Elisa Urda <urda.elisa@gmail.com>
2018-08-20 21:13:58 -0400

    working on getting file structure set up correctly

--- emailtesting/src/components/email.js
+++ emailtesting/src/components/email.js
@@ -1,3 +1,35 @@
 import React from "react";
 import Email from "email-templates";
 
+class Email extends Component {
+    state = {
+        name: "Elisa"
+    }
+
+
+
+    sendEmail = () => {
+        const email = new Email ({
+            message: {
+                from: "urda.elisa@gmail.com"
+            },
+            send: true
+            transport: {
+                jsonTransport: true
+            }
+        });
+    email.send({
+        template: "test",
+        message: {
+            to: "elisaschwarz87@gmail.com"
+        },
+        locals: {
+            name: this.state.name
+        }
+    }).then(console.log)
+    .catch(console.error);
+    };
+
+
+}
+ 
